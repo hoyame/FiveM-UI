ad178139fdbc3de08dd293b4992130fafdfc5da4
hoyame <zou.hoyame@gmail.com>
2022-02-22 12:58:02 +0100

    end

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,9 @@ game { 'gta5' }
 ui_page 'ui.html'
 
 files {
+	'fonts/SF-Pro-Display-Regular.otf',
+	'fonts/SF-Pro-Display-Bold.otf',
+	'fonts/SF-Pro-Display-Semibold.otf',
 	'ui.html',
 	'style.css',
 	'app.js'
