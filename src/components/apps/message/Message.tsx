672144226c728d6e4ff1dbddbabe63555473c6a3
hoyame <zou.hoyame@gmail.com>
2022-03-04 20:38:59 +0100

    update

--- src/components/apps/message/Message.tsx
+++ src/components/apps/message/Message.tsx
@@ -5,13 +5,23 @@ import './Message.scss';
 const Message = () => {
     const [contacts, setContacs] = useState([]);
 
-    const MessageComponent = () => {
+    interface IMessage {
+        name: string;
+        content: string;
+        img?: string;
+        hour: string;
+        notif?: boolean;
+    }
+
+    const MessageComponent = (props: IMessage) => {
         return (
             <div className='list-component'>
+                { props.notif && <div className='list-notif'></div>}
                 <img className="list-image" src="https://avatars.githubusercontent.com/u/56686012?v=4"></img>
                 <div className="list-infos">
-                    <p className="list-name">Hoyame</p>
-                    <p className="list-content">I love youuuu forttt forttt forttt melciaaaa</p>
+                    <p className="list-name">{props.name}</p>
+                    <p className="list-content">{props.content}</p>
+                    <p className="list-hour">il y'a {props.hour}</p>
                 </div>
             </div>
         );  
@@ -22,7 +32,26 @@ const Message = () => {
             <div className="box-t message">
                 <div className='list-messages'>
                     <p className='title'>Messages</p>
-                    <MessageComponent />
+                    <div className="message-scroll">
+                        <MessageComponent name="Hoyame" content="iorghrhgoirbgr" hour="4 heure" notif={true} />
+                        <MessageComponent name="flora" content="iorghrhgoirbgr" hour="4 heure" notif={true} />
+                        <MessageComponent name="luca scilachier" content="iorghrhgoirbgr" hour="4 heure" notif={false} />
+                        <MessageComponent name="romain ton pote la" content="iorghrhgoirbgr" hour="4 heure" notif={true} />
+                        <MessageComponent name="arabiatch" content="iorghrhgoirbgr" hour="4 heure" notif={true} />
+                        <MessageComponent name="amonaillettet" content="eohfeohigeg" hour="2 jour" />
+                        <MessageComponent name="poulet au olives" content="iorghrhgoirbgr" hour="4 heure" notif={true} />
+                        <MessageComponent name="info.gouv <covid>" content="iorghrhgoirbgr" hour="4 heure" notif={true} />
+                        <MessageComponent name="hobbinou" content="iorghrhgoirbgr" hour="4 heure" notif={false} />
+                        <MessageComponent name="christofleure" content="iorghrhgoirbgr" hour="4 heure" notif={true} />
+                        <MessageComponent name="clairwi" content="iorghrhgoirbgr" hour="4 heure" notif={true} />
+                        <MessageComponent name="negets de pouli" content="iorghrhgoirbgr" hour="4 heure" notif={true} />
+                        <MessageComponent name="pour 75ptns un menu beste of offert" content="iorghrhgoirbgr" hour="4 heure" notif={true} />
+                        <MessageComponent name="" content="iorghrhgoirbgr" hour="4 heure" notif={true} />
+                    </div>
+                </div>
+
+                <div className="content-message">
+
                 </div>
             </div>
         </section>
@@ -30,3 +59,4 @@ const Message = () => {
 }
 
 export default Message
+
