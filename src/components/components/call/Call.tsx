bcc8a0d75dae5f5e12f81faecf47430cf3b23980
hoyame <zou.hoyame@gmail.com>
2022-04-03 03:42:50 +0200

    tweak: end call integration

--- src/components/components/call/Call.tsx
+++ src/components/components/call/Call.tsx
@@ -1,34 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchNui, useNuiEvent } from '../../hooks';
 
 import './Call.scss'
 
 interface ICallIncoming {
     name: string;
     number?: string;
+    id: number;
 
     refuse?: () => void;
     accept?: () => void;
 }
 
 export function CallIncoming(props: ICallIncoming) {
+    const [timer, setTimer] = useState(false)
+    const [display, setDisplay] = useState("flex")
+
+    useEffect(() => {
+      setTimeout(() => {
+        setTimer(true)
+
+        setTimeout(() => {
+          setDisplay("none")
+        }, 1100)
+
+        setTimeout(() => {
+            setDisplay("flex")
+            setTimer(false)
+        }, 10000)
+      }, 15000)
+    }, [])
+    
     return (
-        <div className="box-t call">
+        <div className={timer ? "fade-out box-t call" : "box-t call"} style={{display: display}}>
             <p>{props.name}</p>
 
             <div style={{display: "flex", flexDirection: "row"}}>
-            <div className="call-refuse" onClick={props.refuse}>
-                <svg className="call-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 90.859 33.817">
-                <g transform="translate(-6.854 51.611)">
-                    <path d="M52.295-51.611c-16.943,0-32.666,3.516-40.43,11.279a16.21,16.21,0,0,0-4.98,12.891,11.412,11.412,0,0,0,3.027,7.666,6.142,6.142,0,0,0,5.42,1.9L29.59-20.312a8.413,8.413,0,0,0,4.834-2c1.27-1.269,1.563-3.125,1.563-5.517v-3.809a2.143,2.143,0,0,1,.635-1.465,2.558,2.558,0,0,1,1.465-.781A75.793,75.793,0,0,1,52.295-35.01c6.152,0,11.475.439,14.209,1.123a3.054,3.054,0,0,1,1.367.781,1.949,1.949,0,0,1,.635,1.465v3.809c0,2.392.342,4.248,1.611,5.517a8.246,8.246,0,0,0,4.785,2L89.111-17.92a6.3,6.3,0,0,0,5.615-1.9,10.812,10.812,0,0,0,2.979-7.617,16.946,16.946,0,0,0-5.273-12.891C84.619-48.145,69.189-51.611,52.295-51.611Z"/>
-                </g>
-                </svg>
-                
-            </div>
-            <div className="call-accept" onClick={props.accept}>
-                <svg className="call-icon" style={{height: "15px"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 92.382 92.48">
-                <path d="M36.621-12.842c14.4,14.453,31.934,25.586,46.24,25.586a21.312,21.312,0,0,0,16.5-7.227c2.637-2.93,4.248-6.3,4.248-9.668a8.152,8.152,0,0,0-3.515-6.885L85.107-21.68c-2.393-1.66-4.346-2.441-6.1-2.441-2.295,0-4.346,1.269-6.592,3.516L68.9-17.09a2.828,2.828,0,0,1-1.9.781,4.04,4.04,0,0,1-2-.586c-3.076-1.66-8.35-6.152-13.232-11.035S42.334-38.037,40.723-41.162a4.187,4.187,0,0,1-.537-2,2.735,2.735,0,0,1,.732-1.855l3.516-3.564c2.2-2.295,3.516-4.3,3.516-6.592,0-1.807-.781-3.76-2.441-6.1l-10.6-14.844a8.37,8.37,0,0,0-7.129-3.613,13.794,13.794,0,0,0-9.521,4.248,21.952,21.952,0,0,0-7.031,16.6C11.231-44.58,22.217-27.246,36.621-12.842Z" transform="translate(-11.231 79.736)"/>
-                </svg>
-            </div>
+                <div className="call-refuse" onClick={() => {
+                    fetchNui<any>('refuse-incomingcall', { id: props.id })
+                    setTimer(true)
+
+                    setTimeout(() => {
+                      setDisplay("none")
+                    }, 1100)
+            
+                    setTimeout(() => {
+                        setDisplay("flex")
+                        setTimer(false)
+                    }, 10000)
+                }}>
+                    <svg className="call-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 90.859 33.817">
+                    <g transform="translate(-6.854 51.611)">
+                        <path d="M52.295-51.611c-16.943,0-32.666,3.516-40.43,11.279a16.21,16.21,0,0,0-4.98,12.891,11.412,11.412,0,0,0,3.027,7.666,6.142,6.142,0,0,0,5.42,1.9L29.59-20.312a8.413,8.413,0,0,0,4.834-2c1.27-1.269,1.563-3.125,1.563-5.517v-3.809a2.143,2.143,0,0,1,.635-1.465,2.558,2.558,0,0,1,1.465-.781A75.793,75.793,0,0,1,52.295-35.01c6.152,0,11.475.439,14.209,1.123a3.054,3.054,0,0,1,1.367.781,1.949,1.949,0,0,1,.635,1.465v3.809c0,2.392.342,4.248,1.611,5.517a8.246,8.246,0,0,0,4.785,2L89.111-17.92a6.3,6.3,0,0,0,5.615-1.9,10.812,10.812,0,0,0,2.979-7.617,16.946,16.946,0,0,0-5.273-12.891C84.619-48.145,69.189-51.611,52.295-51.611Z"/>
+                    </g>
+                    </svg>
+                    
+                </div>
+                <div className="call-accept" onClick={() => {
+                    fetchNui<any>('accept-incomingcall', { id: props.id })
+                }}>
+                    <svg className="call-icon" style={{height: "15px"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 92.382 92.48">
+                    <path d="M36.621-12.842c14.4,14.453,31.934,25.586,46.24,25.586a21.312,21.312,0,0,0,16.5-7.227c2.637-2.93,4.248-6.3,4.248-9.668a8.152,8.152,0,0,0-3.515-6.885L85.107-21.68c-2.393-1.66-4.346-2.441-6.1-2.441-2.295,0-4.346,1.269-6.592,3.516L68.9-17.09a2.828,2.828,0,0,1-1.9.781,4.04,4.04,0,0,1-2-.586c-3.076-1.66-8.35-6.152-13.232-11.035S42.334-38.037,40.723-41.162a4.187,4.187,0,0,1-.537-2,2.735,2.735,0,0,1,.732-1.855l3.516-3.564c2.2-2.295,3.516-4.3,3.516-6.592,0-1.807-.781-3.76-2.441-6.1l-10.6-14.844a8.37,8.37,0,0,0-7.129-3.613,13.794,13.794,0,0,0-9.521,4.248,21.952,21.952,0,0,0-7.031,16.6C11.231-44.58,22.217-27.246,36.621-12.842Z" transform="translate(-11.231 79.736)"/>
+                    </svg>
+                </div>
             </div>
         </div>
     )
@@ -37,14 +71,57 @@ export function CallIncoming(props: ICallIncoming) {
 interface IInCall {
     name: string;
     end?: () => void;
+    id: number;
 }
 
 export function InCall(props: IInCall) {
+    const [timer, setTimer] = useState(false)
+    const [display, setDisplay] = useState("flex")
+
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] =  useState(1);
+
+    useEffect(()=>{
+        let myInterval = setInterval(() => {
+            setSeconds(seconds + 1);
+            
+            if (seconds === 60) {
+                setMinutes(minutes + 1);
+                setSeconds(0);
+            } 
+        }, 1000)
+
+        return ()=> {
+            clearInterval(myInterval);
+        };
+    }, [minutes, seconds]);
+
+    useNuiEvent<any>('end-incall', (s: any) => {
+        if (s.id == props.id) {
+            endCall()
+        }
+    })
+
+    const endCall = () => {
+        setTimer(true)
+    
+        setTimeout(() => {
+          setDisplay("none")
+        }, 1100)
+    
+        setTimeout(() => {
+            setDisplay("flex")
+            setTimer(false)
+            setMinutes(0)
+            setSeconds(0)
+        }, 10000)
+    }
+
     return (
-        <div className="box-t call">
+        <div className={timer ? "fade-out box-t call" : "box-t call"} style={{display: display}}>
             <div>
                 <p>{props.name}</p>
-                <p style={{fontSize: "11px", fontFamily: 'SF-Pro-Text-Regular', opacity: "0.6"}}>00:02</p>
+                <p style={{fontSize: "11px", fontFamily: 'SF-Pro-Text-Regular', opacity: "0.6"}}>{minutes < 10 ? "0" + minutes : minutes}:{seconds < 10 ? "0" + seconds : seconds}</p>
             </div>
 
             <div style={{display: "flex", flexDirection: "row"}}>
@@ -54,7 +131,9 @@ export function InCall(props: IInCall) {
                     </svg>  
                 </div>
 
-                <div className="call-refuse" style={{marginRight: 0}} onClick={props.end}>
+                <div className="call-refuse" style={{marginRight: 0}} onClick={() => {
+                    fetchNui<any>('end-call', { id: props.id })
+                }}>
                     <svg className="call-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 90.859 33.817">
                         <g transform="translate(-6.854 51.611)">
                         <path d="M52.295-51.611c-16.943,0-32.666,3.516-40.43,11.279a16.21,16.21,0,0,0-4.98,12.891,11.412,11.412,0,0,0,3.027,7.666,6.142,6.142,0,0,0,5.42,1.9L29.59-20.312a8.413,8.413,0,0,0,4.834-2c1.27-1.269,1.563-3.125,1.563-5.517v-3.809a2.143,2.143,0,0,1,.635-1.465,2.558,2.558,0,0,1,1.465-.781A75.793,75.793,0,0,1,52.295-35.01c6.152,0,11.475.439,14.209,1.123a3.054,3.054,0,0,1,1.367.781,1.949,1.949,0,0,1,.635,1.465v3.809c0,2.392.342,4.248,1.611,5.517a8.246,8.246,0,0,0,4.785,2L89.111-17.92a6.3,6.3,0,0,0,5.615-1.9,10.812,10.812,0,0,0,2.979-7.617,16.946,16.946,0,0,0-5.273-12.891C84.619-48.145,69.189-51.611,52.295-51.611Z"/>
